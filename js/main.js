44eacf1ce873676f3d27101b7b9da3921644525f
aptkf <a.p.petcoff@gmail.com>
2014-03-17 00:44:52 -0400

    adds ajax contact form functionality

--- js/main.js
+++ js/main.js
@@ -24,8 +24,43 @@ function navLinks() {
   }).scroll();
 }
 
+function contactForm() {
+  $(".contact form").submit(function(e) {
+    e.preventDefault();
+    var name,email,subject,msg,dataString;
+    name = $(".contact input[name^=\"name\"]").val();
+    email = $(".contact input[name^=\"email\"]").val();
+    subject = $(".contact input[name^=\"subject\"]").val();
+    message = $(".contact textarea[name^=\"message\"]").val();
+    dataString = 'name=' + name + '&email=' + email + '&message=' + message;
+
+    if(subject==false) {
+      $.ajax({
+        type: "POST",
+        url: "http://getsimpleform.com/messages/ajax?form_api_token=b5f5dc858da495c26b344e4fa8b484b3",
+        dataType: 'jsonp',
+        data: dataString,
+        success: function() {
+          $(".success").css("display","block");
+          $(".contact input").val("");
+          $(".contact textarea").val("");
+        },
+        error: function() {
+          $(".error").css("display","block");
+        }
+      });
+    } else {
+      $(".error").css("display","block");
+      return false;
+    }
+  });
+}
+
 $(document).ready(function(){
   $("html").removeClass("no-trs");
+  $(".contact form ol li:nth-child(3)").css("display","none");
 
   navLinks();
+
+  contactForm();
 });
