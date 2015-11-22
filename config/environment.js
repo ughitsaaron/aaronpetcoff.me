0811639683ecb0d1c7abad1a96b42f8a541ffa73
Aaron Petcoff <hello@aaronpetcoff.me>
2015-11-22 15:02:27 -0500

    adds disqus commenting to posts

--- config/environment.js
+++ config/environment.js
@@ -13,6 +13,10 @@ module.exports = function(environment) {
       }
     },
 
+    disqus: {
+      shortname: 'aaronpetcoffme'
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
