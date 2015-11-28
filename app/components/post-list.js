2a46d45fe8d243282a5790a6f341bbec412f8749
Aaron Petcoff <hello@aaronpetcoff.me>
2015-11-28 15:28:32 -0500

    adds structured data

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -4,5 +4,8 @@ export default Ember.Component.extend({
   tagName:'ul',
   classNames: ['post-list'],
   order: ['created:desc'],
+  attributeBindings: ['itemscope', 'itemtype'],
+  itemscope: 'itemscope',
+  itemtype: 'https://schema.org/Blog',
   posts: Ember.computed.sort('data', 'order')
 });
