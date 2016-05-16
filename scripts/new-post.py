936d91fd661a830db192d382a8916210600e09b6
Aaron Petcoff <hello@aaronpetcoff.me>
2016-05-15 22:34:37 -0400

    description

--- scripts/new-post.py
+++ scripts/new-post.py
@@ -7,9 +7,6 @@ print 'Creating new post...'
 
 title = raw_input('Enter a title: ')
 
-filename = str(date.today()) + '-' + title
-file = open('posts/{}.md'.format(filename), 'w')
+filename = '{}-{}'.format(str(date.today()), title.replace(' ', '-'))
 
-file.write('---\ntitle: {}\n---'.format(title.replace(' ', '-')))
-
-file.close()
+open('posts/{}.md'.format(filename), 'w').write('---\ntitle: {}\n---'.format(title))
