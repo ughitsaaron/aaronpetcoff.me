8acd4d77b72fa04c0bbaee85f01a79ce8e79607a
Aaron Petcoff <hello@aaronpetcoff.me>
2016-06-03 16:28:25 -0400

    changes date in post list

--- app/models/post.js
+++ app/models/post.js
@@ -1,9 +1,16 @@
 import DS from 'ember-data';
+import moment from 'moment';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   created: DS.attr('date'),
   body: DS.attr('string'),
   tags: DS.attr(),
-  slugs: DS.attr()
+  slugs: DS.attr(),
+
+  fromNow: Ember.computed('created', function () {
+    const created = this.get('created');
+
+    return moment().diff(created, 'days') < 1 ? 'earlier today' : moment(created).fromNow();
+  })
 });
