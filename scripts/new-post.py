3a344c5927736be47f0c4bf162b397c22f6cad9a
Aaron Petcoff <hello@aaronpetcoff.me>
2016-06-04 16:55:59 -0400

    update new post script

--- scripts/new-post.py
+++ scripts/new-post.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python
 
 from datetime import date
-import sys
 
-print 'Creating new post...'
+def dump(ls):
+    s = ''
+    for e in ls:
+        s += '\n  - {}'.format(e)
+    return s
 
-title = raw_input('Enter a title: ')
+# Enter a title...
+title = input('Enter a title: ')
+assert len(title) > 0, 'A title is required'
+assert isinstance(title, str), 'Response {} is not a string'.format(title)
 
-filename = '{}-{}'.format(str(date.today()), title.replace(' ', '-'))
+# Write as draft?...
+draft = input('Write as draft? [y/n]: ')
+assert (draft == 'y' or draft == 'n'), 'Answer {} is not: \'y\' or \'n\''.format(draft)
 
-open('posts/{}.md'.format(filename), 'w').write('---\ntitle: {}\n---'.format(title))
+# Tags
+tags_raw = input('Tags (Separated by comma and space): ')
+
+filename = '{}-{}'.format(str(date.today()), title.replace(' ', '-').lower())
+directory = 'drafts' if 'y' else 'posts'
+
+tags = dump(tags_raw.split(', '))
+
+meta = """---
+title: {}
+tags: {}
+---""".format(title, tags if len(tags_raw) > 0 else '[]')
+
+# Write new file
+print('Creating new post...')
+open('{}/{}.md'.format(directory, filename), 'w').write(meta)
