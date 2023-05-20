44c80cc1a76f9cc8b96f0945cc4ab0c80e31d26a
Aaron Petcoff <a.p.petcoff@gmail.com>
2023-05-20 17:14:39 -0400

    even more config

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import styled from 'styled-components';
 import '../style.css';
 
 const Layout = ({ children }) => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { useStaticQuery, graphql } from 'gatsby';
-import styled from 'styled-components';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import type { PageProps } from 'gatsby';
